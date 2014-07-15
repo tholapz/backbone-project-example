65c7c7cc0ab041a52ff35f91bcba1bc6c4b9da4f
Kamol Treewatchararat <Kamol.Treewatchararat@telescope.tv>
2014-07-15 14:13:51 -0700

    init commit

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global TholapzCom, $*/
+
+
+window.TholapzCom = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    TholapzCom.init();
+});
